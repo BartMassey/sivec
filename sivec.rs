50b954afd88cdc347d540b4b885d3f5cbe43e555
Bart Massey <bart@cs.pdx.edu>
2018-03-23 17:08:57 -0700

    added license

--- sivec.rs
+++ sivec.rs
@@ -1,4 +1,9 @@
 #![feature(alloc, allocator_api, core_intrinsics)]
+// Copyright © 2018 Bart Massey
+// [This program is licensed under the "MIT License"]
+// Please see the file LICENSE in the source
+// distribution of this software for license terms.
+
 extern crate alloc;
 use alloc::raw_vec::RawVec;
 use alloc::heap::Heap;
